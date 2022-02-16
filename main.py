204e313ad988afaec5703a85222a6d5458dd0f75
Paulina Baeumker <baeumker@big5.de>
2022-02-16 09:39:01 +0100

    Created list and user json files that will contain the data

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from flask import request
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# read files
+with open('./data/user.json') as json_file:
+    user_data = json.load(json_file)
+
+with open('./data/lists.json') as json_file:
+    list_data = json.load(json_file)
+
+@app.route('/list', methods=['POST'])
+def get_lists():
+    data = json.load(json_file)
+    return data
+
+@app.route('/list/<list_id>', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return xxx
+    else:
+        return xxx
